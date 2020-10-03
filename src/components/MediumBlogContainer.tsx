2010d119eb78cfde50c8197df399e15eb41b4bff
heena singh <49567902+evanessa234@users.noreply.github.com>
2020-10-03 21:33:55 +0530

    feat: add a Blog Page (#23)

---
+++ src/components/MediumBlogContainer.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const MediumBlogContainer = styled.div`
+	position: relative;
+	top: 
+	left: 0;
+	bottom: 0;
+	right: 0;
+	margin: 0;
+	overflow: hidden;
+`;
+
+export default MediumBlogContainer;
